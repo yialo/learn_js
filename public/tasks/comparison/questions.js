dca1daf825ad14daebe4546abb90359de740da59
Aleksei Arro <polsck@gmail.com>
2020-01-12 22:18:28 +0300

    Scripts formatted, current script added to index.html

--- public/tasks/comparison/questions.js
+++ public/tasks/comparison/questions.js
@@ -2,9 +2,9 @@
 'use strict';
 
 console.log(5 > 4 === true);
-console.log("ананас" > "яблоко" === false);
-console.log("2" > "12" === true);
+console.log('ананас' > 'яблоко' === false);
+console.log('2' > '12' === true);
 console.log((undefined == null) === true);
 console.log((undefined === null) === false);
-console.log((null == "\n0\n") === false);
-console.log((null === +"\n0\n") === false);
+console.log((null == '\n0\n') === false);
+console.log((null === +'\n0\n') === false);
--- public/tasks/type-conversions/questions.js
+++ public/tasks/type-conversions/questions.js
@@ -1,17 +1,17 @@
 // https://learn.javascript.ru/task/primitive-conversions-questions
 'use strict';
 
-console.log("" + 1 + 0 === "10");
-console.log("" - 1 + 0 === -1);
+console.log('' + 1 + 0 === '10');
+console.log('' - 1 + 0 === -1);
 console.log(true + false === 1);
-console.log(6 / "3" === 2);
-console.log("2" * "3" === 6);
-console.log(4 + 5 + "px" === "9px");
-console.log("$" + 4 + 5 === "$45");
-console.log("4" - 2 === 2);
-console.log(Number.isNaN("4px" - 2) === true);
+console.log(6 / '3' === 2);
+console.log('2' * '3' === 6);
+console.log(4 + 5 + 'px' === '9px');
+console.log('$' + 4 + 5 === '$45');
+console.log('4' - 2 === 2);
+console.log(Number.isNaN('4px' - 2) === true);
 console.log(7 / 0 === Infinity);
-console.log("  -9  " + 5 === "  -9  5");
-console.log("  -9  " - 5 === -14);
+console.log('  -9  ' + 5 === '  -9  5');
+console.log('  -9  ' - 5 === -14);
 console.log(null + 1 === 1);
 console.log(Number.isNaN(undefined + 1) === true);
