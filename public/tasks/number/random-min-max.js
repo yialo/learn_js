7f259fb71f4d1f61fe23eab91a105ece023661ad
Aleksei Arro <polsck@gmail.com>
2020-08-29 15:48:19 +0300

    Add JSDoc annotation to random-min-max task

--- public/tasks/number/random-min-max.js
+++ public/tasks/number/random-min-max.js
@@ -1,5 +1,9 @@
 'use strict';
 
+/**
+ * @see https://learn.javascript.ru/task/random-min-max
+ */
+
 function random(min, max) {
   const delta = Math.abs(max - min);
   return min + delta * Math.random();
