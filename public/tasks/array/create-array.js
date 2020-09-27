69aa1d74fdcb63de116b5842666e1c3e0b33db95
Aleksei Arro <polsck@gmail.com>
2020-09-27 18:51:02 +0300

    Update 'create-array' task solution

--- public/tasks/array/create-array.js
+++ public/tasks/array/create-array.js
@@ -7,7 +7,10 @@
 exports.createArray = () => {
   const styles = ['Джаз', 'Блюз'];
   styles.push('Рок-н-ролл');
+
+  const centerNumber = Math.floor((styles.length - 1) / 2)
   styles[1] = 'Классика';
+
   console.log(styles.shift());
   styles.unshift('Рэп', 'Регги');
   return styles;
