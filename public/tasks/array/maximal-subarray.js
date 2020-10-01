2f83c71a136840762b2bd4979f2ff387a2d822e7
Aleksei Arro <polsck@gmail.com>
2020-10-02 00:23:07 +0300

    Complete maximal-subarray task with O(n^2) speed

--- public/tasks/array/maximal-subarray.js
+++ public/tasks/array/maximal-subarray.js
@@ -4,7 +4,30 @@
  * @see {@link https://learn.javascript.ru/task/maximal-subarray}
  */
 
-exports.getMaxSubSum = (arr) => {
+const getArraySum = (arr) => arr.reduce((acc, it) => acc + it);
 
-  return null;
+exports.getMaxSubSum = (arr) => {
+  if (arr.every(el => el < 0) ) {
+    return 0;
+  }
+
+  const subArrs = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j <= arr.length; j++) {
+      const subArr = arr.slice(i, j);
+      subArrs.push(subArr);
+    }
+  }
+
+  let maxSum = 0;
+
+  for (let i = 0; i < subArrs.length; i++) {
+    const nextSum = getArraySum(subArrs[i]);
+    if (nextSum > maxSum) {
+      maxSum = nextSum;
+    }
+  }
+
+  return maxSum;
 };
