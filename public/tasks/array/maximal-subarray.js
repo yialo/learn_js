eec3379e19d5a8735e6cc41a98def1ca56dfc1ee
Aleksei Arro <polsck@gmail.com>
2020-10-02 00:59:08 +0300

    Add solution of maximal-subarray with O(n) speed

--- public/tasks/array/maximal-subarray.js
+++ public/tasks/array/maximal-subarray.js
@@ -5,22 +5,37 @@
  */
 
 exports.getMaxSubSumSlow = (arr) => {
-  if (arr.every(el => el < 0) ) {
+  if (arr.every(it => it < 0) ) {
     return 0;
   }
 
   let maxSum = 0;
 
   for (let i = 0; i < arr.length; i++) {
-    let subSum = 0;
+    let partialSum = 0;
 
     for (let j = i; j < arr.length; j++) {
-      subSum += arr[j];
-      maxSum = Math.max(maxSum, subSum);
+      partialSum += arr[j];
+      maxSum = Math.max(maxSum, partialSum);
     }
   }
 
   return maxSum;
 };
 
-exports.getMaxSubSumFast = (arr) => {};
+exports.getMaxSubSumFast = (arr) => {
+  let maxSum = 0;
+  let partialSum = 0;
+
+  for (const item of arr) {
+    partialSum += item;
+
+    if (partialSum >= 0) {
+      maxSum = Math.max(maxSum, partialSum);
+    } else {
+      partialSum = 0;
+    }
+  }
+
+  return maxSum;
+};
--- public/tasks/array/maximal-subarray.test.js
+++ public/tasks/array/maximal-subarray.test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const { getMaxSubSumSlow: getMaxSubSum } = require('./maximal-subarray');
+const { getMaxSubSumSlow, getMaxSubSumFast } = require('./maximal-subarray');
 
-describe('getMaxSubSum', () => {
+describe('getMaxSubSumSlow', () => {
   test.each([
     [[-1, 2, 3, -9], [2, 3], 5],
     [[2, -1, 2, 3, -9], [2, -1, 2, 3], 6],
@@ -12,7 +12,22 @@ describe('getMaxSubSum', () => {
     [[1, 2, 3], [1, 2, 3], 6],
     [[-1, -2, -3], [], 0],
   ])('should just work', (arr, _subArr, expectedSum) => {
-    const result = getMaxSubSum(arr);
+    const result = getMaxSubSumSlow(arr);
+    expect(result).toBe(expectedSum);
+  });
+});
+
+describe('getMaxSubSumFast', () => {
+  test.each([
+    [[-1, 2, 3, -9], [2, 3], 5],
+    [[2, -1, 2, 3, -9], [2, -1, 2, 3], 6],
+    [[-1, 2, 3, -9, 11], [11], 11],
+    [[-2, -1, 1, 2], [1, 2], 3],
+    [[100, -9, 2, -3, 5], [100], 100],
+    [[1, 2, 3], [1, 2, 3], 6],
+    [[-1, -2, -3], [], 0],
+  ])('should just work', (arr, _subArr, expectedSum) => {
+    const result = getMaxSubSumFast(arr);
     expect(result).toBe(expectedSum);
   });
 });
