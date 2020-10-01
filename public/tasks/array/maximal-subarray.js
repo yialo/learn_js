374b1be58448a59c21c2d45985c4c2d067e356bd
Aleksei Arro <polsck@gmail.com>
2020-10-02 00:37:34 +0300

    Avoid double walkthrough of array

--- public/tasks/array/maximal-subarray.js
+++ public/tasks/array/maximal-subarray.js
@@ -4,27 +4,20 @@
  * @see {@link https://learn.javascript.ru/task/maximal-subarray}
  */
 
-const getArraySum = (arr) => arr.reduce((acc, it) => acc + it);
-
 exports.getMaxSubSumSlow = (arr) => {
   if (arr.every(el => el < 0) ) {
     return 0;
   }
 
-  const subArrs = [];
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i + 1; j <= arr.length; j++) {
-      const subArr = arr.slice(i, j);
-      subArrs.push(subArr);
-    }
-  }
-
   let maxSum = 0;
 
-  for (let i = 0; i < subArrs.length; i++) {
-    const nextSum = getArraySum(subArrs[i]);
-    maxSum = Math.max(maxSum, nextSum);
+  for (let i = 0; i < arr.length; i++) {
+    let subSum = 0;
+
+    for (let j = i; j < arr.length; j++) {
+      subSum += arr[j];
+      maxSum = Math.max(maxSum, subSum);
+    }
   }
 
   return maxSum;
